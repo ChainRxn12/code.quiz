d1f306d12a110530586113d5162afca8a2bfea29
Billy Dorman <b.d.dorman12@gmail.com>
2021-05-18 14:58:13 -0500

    adding script.js, timer function, variables, etc.

---
+++ script.js
@@ -0,0 +1,35 @@
+//assign variables
+var startGame = document.querySelector("#btn");
+var timerElement = document.querySelector(".timer");
+var secondsLeft = 120;
+var questions = ["1?", "2?", "3?", "4?"];
+var answers;
+
+
+//listeners
+startGame.addEventListener("click", function() {
+    secondsLeft--;
+    getRandomQuestion();
+    setTime();
+    logKey(); 
+});
+//random question function not working*****
+function getRandomQuestion(){
+    i = Math.floor(Math.random() * questions.length);
+    ranQuestion = questions[i];
+    console.log(ranQuestion);
+    const log = document.getElementById('log');
+}    
+
+
+//timer function is working
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerElement.textContent = secondsLeft + " time remaining";
+        if(secondsLeft=== 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
+}
