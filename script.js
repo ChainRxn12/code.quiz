e46cc6b6f9d4cd634e9fe04dafe2490d8eb08d01
Billy Dorman <b.d.dorman12@gmail.com>
2021-05-19 22:27:01 -0500

    developed 4 questions, answers, wrong answers in script.js

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ var secondsLeft = 120;
 var questions = ["1?", "2?", "3?", "4?"];
 var answers = 
 
+/*Question 1: What does the <p> indicate?
+Answer: Paragraph Tag
+Wrong Answers: Parent Tag, Push Value, The Color Purple*/
+
+/*Question 2: What does CSS stand for?
+Answer: Cascading Style Sheet
+Wrong Answers: Cosmic Star Ship, Colorado Six Shooters, Cascade Starting Sheet*/
+
+/*Question 3: Which of the following is NOT a programming language?
+Answer: LatteScipt
+Wrong Answers: JavaScript, Java, Python
+
+/*Question 4: What best describes the term "bug" while coding?
+Answer: A mistake
+Wrong Answers: A Command, Something Icky, A Loop*/
+
 
 //listeners
 startGame.addEventListener("click", function() {
